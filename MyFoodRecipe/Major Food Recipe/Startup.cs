555d18884d775120b4792fdad77ce59d9ba77448
Amol Gahane <amolg10@birlasoft.com>
2022-09-20 11:17:42 +0530

    Initial commit

--- MyFoodRecipe/Major Food Recipe/Startup.cs
+++ MyFoodRecipe/Major Food Recipe/Startup.cs
@@ -1,6 +1,11 @@
+using Major_Food_Recipe.Data;
+using Major_Food_Recipe.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,7 +28,48 @@ namespace Major_Food_Recipe
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                string connDb = Configuration.GetConnectionString("DefaultConnection");
+                options.UseSqlServer(connDb);
+
+            });
+
+            //Register the OWIN Identity Middlware
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = true;
+                options.Password.RequiredLength = 8;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders(); /*FOR SENDING TOKENS*/
+
+            //Register ASP.NET Razor Page Middleware
+            services
+                .AddRazorPages()
+                .AddRazorPagesOptions(options =>
+                {
+                    options.Conventions.AuthorizeAreaFolder("Identity", "/Account/Manage");
+                    options.Conventions.AuthorizeAreaPage("Identity", "/Account/Logout");
+                });
+
+            //Configure the Application Cookie options
+            services
+                .ConfigureApplicationCookie(options =>
+                {
+                    options.LoginPath = "/Identity/Account/Login";
+                    options.LogoutPath = "/Identity/Account/Logout";
+                    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(25);
+                    options.SlidingExpiration = true; //for prevent to data loss
+                    options.Cookie.HttpOnly = true;
+                    options.Cookie.Name = "FoodHubCookie";
+                });
+
+            //Register the EmailSender Service fo Dependency Injection
+            services
+                .AddSingleton<IEmailSender, MyEmailSenderService>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,11 +91,25 @@ namespace Major_Food_Recipe
 
             app.UseRouting();
 
+            //Activate the OWIN Middleware for Authentication
+            app.UseAuthentication();/*ADDED BY MYSELF*/
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
+
                 endpoints.MapRazorPages();
+                //Register  routes for in areas
+                endpoints.MapControllerRoute(
+                name:"areas",
+                pattern: "{area}/{controller=Home}/{action=Index}/{id?}");
+
+                //Register routes for not in areas
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
             });
         }
     }
